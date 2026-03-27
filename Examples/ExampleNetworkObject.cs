8532010a2975e683807a737fa698f08934d093a2
Carsten23455 <carstencorbin2008@gmail.com>
2026-03-27 00:59:26 -0500

    Update ExampleNetworkObject.cs

--- Examples/ExampleNetworkObject.cs
+++ Examples/ExampleNetworkObject.cs
@@ -1,1 +1,18 @@
+using MessHallAPI.Networking;
+using UnityEngine;
 
+namespace MessHallAPIExample.Manager
+{
+    public class NetworkObjectManagerExample
+    {
+        ///<summary>
+        ///Example object with MessHallNetworkTransform
+        ///</summary>
+        public static void test1()
+        {
+            GameObject objtest = new GameObject();
+            objtest.AddComponent<MessHallNetworkTransform>();
+            objtest.GetComponent<MessHallNetworkTransform>().Initialize(objtest.name);
+        }
+    }
+}
